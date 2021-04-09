82815f42a6210119b7fd80fa8b72e2aac71c1c17
ahmed mohamed <ama.32097@gmail.com>
2021-04-10 01:36:52 +0200

    footer-finished

--- footer.php
+++ footer.php
@@ -1,4 +1,61 @@
-<div>footer goes here</div>
+<footer class="bg-primary">
+    <div class="m-2 text-white divide-y divide-gray-400 ">
+        <div class="grid md:grid-cols-4 sm:grid-cols-2">
+            <div class="m-1">
+                <h4 class="pb-1 footer-title">CATEGORIES</h4>
+                <ul class="mt-1 text-sm">
+                    <li><a class="footer-list">Best Seller</a></li>
+                </ul>
+            </div>
+            <div class="m-1">
+                <h4 class="pb-1 footer-title">INFORMATION</h4>
+                <ul class="mt-1 text-sm">
+                    <li><a class="footer-list">About Us</a></li>
+                </ul>
+            </div>
+            <div class="m-1">
+                <h4 class="pb-1 footer-title">MY ACCOUNT</h4>
+                <ul class="mt-1 text-sm">
+                    <li><a class="footer-list">Account</a></li>
+                </ul>
+            </div>
+            <div class="m-1">
+                <h4 class="pb-1 footer-title">CONTACT US</h4>
+                <div class="grid grid-cols-4 w-28">
+                    <a href="#"><img class="social-icon" src="https://www.flaticon.com/svg/vstatic/svg/49/49354.svg?token=exp=1618004463~hmac=22656202982e050a2129004588c1d6b2"></a>
+                    <a href="#"><img class="social-icon" src="https://www.flaticon.com/svg/vstatic/svg/1384/1384017.svg?token=exp=1618004339~hmac=680cbd13aaed524a294d5b181999fdad"></a>
+                    <a href="#"><img class="social-icon" src="https://www.flaticon.com/svg/vstatic/svg/1384/1384012.svg?token=exp=1618004440~hmac=0457f9b90ac1f196e140865404a8a36f"></a>
+                    <a href="#"><img class="social-icon" src="https://www.flaticon.com/svg/vstatic/svg/1384/1384015.svg?token=exp=1618004490~hmac=51bcfd16897b07dadfe9487b0be91a97"></a>
+                </div>
+                <div class="mb-3">
+                    <h1 class="my-2 text-sm">Subscribe To Our Newslater</h1>
+                    <input class="w-48 text-red-500 border border-red-500 rounded-md " name="email" type="email" placeholder="example@email.com" >
+                    <input class="font-bold text-red-500 border border-red-500 rounded-full cursor-pointer w-14 hover:bg-red-500 hover:text-white" type="submit" value="Join">
+                    <p><strong class="text-red-600">Hotline:</strong></p>
+                    <p><strong class="text-red-600">Email:</strong></p>
+                </div>
+                
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+        <div class="">
+            <nav class="grid items-center justify-center grid-flow-col gap-4 mt-3 mb-6 text-center auto-cols-auto">
+                <a class="hover:text-yellow-300" href="#">Home</a>
+                <a class="hover:text-yellow-300" href="#">Blog</a>
+                <a class="hover:text-yellow-300" href="#">Shop</a>
+                <a class="hover:text-yellow-300" href="#">About Us</a>
+                <a class="hover:text-yellow-300" href="#">Contact Us</a>
+            </nav>
+            <div>
+                <p class="text-center">©2021 Zumra. All rights reserved</p>
+            </div>
+        </div>
+    </div>
+
+
+</footer>
 <?php wp_footer() ?>
 
 </body>
