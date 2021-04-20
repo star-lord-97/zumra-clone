6e8837b936c07c0081a0f0cee39ce8828d6d4823
ahmed mohamed <ama.32097@gmail.com>
2021-04-21 01:47:39 +0200

    initial archive-product

--- archive-product.php
+++ archive-product.php
@@ -1,1 +1,138 @@
-<?= 'shop' ?>
+<?php 
+get_header();
+
+?>
+<main class="bg-gray-100">
+    <!-- product navigation -->
+    <div class="m-4" >
+        <nav class="grid justify-start grid-flow-col gap-4 text-justify" id="page-nav">
+            <a class="hover:underline" href="<?php echo site_url();?>" id="home link">Home</a>
+            <p class="text-red-500" id="category name">
+                All Products
+            </p>
+        </nav>
+    </div>
+    <div class="grid grid-flow-col gap-4 m-4">
+        <div>
+            <!-- All Categoires Panel-->
+            <div class="grid grid-flow-row divide-y divide-gray-400">
+                <a href="<?php echo site_url('/products'); ?>" class="text-center text-white bg-gray-500">ALL CATEGORIES</a>
+                <?php 
+                    $categories = get_categories(array(
+                        'taxonomy' => 'product-category',
+                        'orderby' => 'name',
+                        'order' => 'ASC', 
+                        'parent' => 0,
+                        'hide_empty' => 0 //change to 1 to hide categores not having a single post
+                    ));
+                    foreach($categories as $categorie){
+                ?>
+                <a class="text-center hover:text-red-500" href="<?php echo get_term_link($categorie->term_id, $categorie->taxonomy);?>">
+                    <?php echo $categorie->name; 
+                    };
+                    ?>
+                </a>
+
+            </div>
+            <!-- filter by price -->
+            <h1>FILTER BY PRICE</h1>
+            <form method="get" action="">
+                <div id="slider-handles"> 
+                </div>
+                <div id="slider-value">
+                    
+                </div>
+                <input name="lower" class="hidden" id="lower">
+                <input name="upper" class="hidden" id="upper">
+                <button class="w-full text-white bg-red-500 " id="filter-btn" type="submit">FILTER</button>
+            </form>
+        </div>
+        <div>
+            <!-- Sort-by panel -->
+            <div>
+                <form method="get" action="">
+                    <select name="orderby" >
+                        <option value="ASC" >sort by price low to high</option>
+                        <option value="DESC" >sort by price high to low</option>
+                        <input name="lower" class="hidden" id="lower">
+                        <input name="upper" class="hidden" id="upper">
+                    </select>
+                    <input id="sort-btn" type="submit" value="submit">
+                <form>
+            </div>
+            <!-- all products -->
+            <div class="grid grid-cols-4 gap-4">
+                <?php 
+                // filter between prices
+                if (isset($_GET['upper']) && isset($_GET['lower'])){
+                    $upper = $_GET['upper'];
+                    $lower = $_GET['lower'];
+                    if (isset($_GET['orderby'])){
+                        $priceSort = $_GET['orderby'];
+                        $results = new WP_Query(array(
+                            'posts_per_page' => 4,
+                            'post_type' => 'product',
+                            'meta_key' => 'price',
+                            'orderby' => 'meta_value_num',
+                            'order' => $priceSort,
+                            'meta_query' => array(array(
+                                'key' => 'price',
+                                'value' =>  array($lower,$upper),
+                                'compare' => 'BETWEEN',
+                                'type' => 'NUMERIC'
+                            )
+                            )
+                        )
+                        );
+                    }else{
+                        $results = new WP_Query(array(
+                            'posts_per_page' => 4,
+                            'post_type' => 'product',
+                            'meta_query' => array(array(
+                                'key' => 'price',
+                                'value' =>  array($lower,$upper),
+                                'compare' => 'BETWEEN',
+                                'type' => 'NUMERIC'
+                            )
+                            )
+                        )
+                        );
+                    }
+                    
+                    while($results->have_posts()){
+                        $results->the_post();
+                        get_template_part('templates/product-item', null, array(
+                            'title' => get_the_title(),
+                            'image' => get_the_post_thumbnail_url(null, 'product-thumbnail'),
+                            'rate' => 4.7,
+                            'price' => get_field('price'),
+                            'on_sale' => get_field('on_sale'),
+                            'sale_price' => get_field('sale_price')
+                        ));
+                    }
+                }else{
+                    while(have_posts()){
+                        the_post();
+                        get_template_part('templates/product-item', null, array(
+                            'title' => get_the_title(),
+                            'image' => get_the_post_thumbnail_url(null, 'product-thumbnail'),
+                            'rate' => 4.7,
+                            'price' => get_field('price'),
+                            'on_sale' => get_field('on_sale'),
+                            'sale_price' => get_field('sale_price')
+                        ));
+                    }
+
+                }
+                
+                ?>
+
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?php 
+get_footer();
+?>
--- templates/product-item.php
+++ templates/product-item.php
@@ -1,28 +1,48 @@
-<a href="<?= $args['permalink'] ?>" class="bg-white border rounded-b-xl flex flex-col justify-center items-center space-y-4 hover:bg-gray-100">
-    <img src="<?= $args['image'] ?>" alt="" class="w-full h-full">
-    <h1><?= $args['title'] ?></h1>
-    <div class="flex space-x-1 md:space-x-2">
-        <?php
-        for ($i = 0; $i < $args['rate']; $i++) {
-        ?>
-            <svg class="fill-current text-yellow-300 md:h-6 md:w-6 h-5 w-5" viewBox="0 0 20 20">
-                <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-            </svg>
-        <?php
-        }
-        for ($i = 5; $i > $args['rate']; $i--) {
-        ?>
-            <svg class="fill-current text-gray-400 md:h-6 md:w-6 h-5 w-5" viewBox="0 0 20 20">
-                <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-            </svg>
-        <?php
-        }
-        ?>
+
+<a href="<?php echo get_the_permalink();?>" class="flex flex-col items-center justify-center space-y-4 bg-white border rounded-b-xl hover:bg-gray-100">
+    <div class="w-full" id="product card">
+        <img src="<?= $args['image'] ?>" alt="" class="w-full">
+        <h1 class="flex justify-center font-bold text-gray-600" id="product-name"><?php echo $args['title']; ?></h1>
+        <div class="flex justify-center pt-2" id="rating">
+            <div class="stars-outer">
+                <div class="stars-inner"></div>
+            </div>
+        </div>
     </div>
-    <div class="w-full flex justify-evenly items-center pb-4">
+    <div class="flex items-center w-full pb-4 justify-evenly">
         <h1 class="font-bold"><?= $args['price'] ?> EGP</h1>
         <?php if ($args['on_sale']) { ?>
             <h1 class="font-bold line-through"><?= $args['sale_price'] ?> EGP</h1>
         <?php } ?>
     </div>
 </a>
+<!--
+<a href="<?= $args['permalink'] ?>" class="flex flex-col items-center justify-center space-y-4 bg-white border rounded-b-xl hover:bg-gray-100">
+    <img src="<?= $args['image'] ?>" alt="" class="w-full h-full">
+    <h1><?= $args['title'] ?></h1>
+    <div class="flex space-x-1 md:space-x-2">
+        <?php
+        for ($i = 0; $i < $args['rate']; $i++) {
+        ?>
+            <svg class="w-5 h-5 text-yellow-300 fill-current md:h-6 md:w-6" viewBox="0 0 20 20">
+                <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+            </svg>
+        <?php
+        }
+        for ($i = 5; $i > $args['rate']; $i--) {
+        ?>
+            <svg class="w-5 h-5 text-gray-400 fill-current md:h-6 md:w-6" viewBox="0 0 20 20">
+                <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+            </svg>
+        <?php
+        }
+        ?>
+    </div>
+    <div class="flex items-center w-full pb-4 justify-evenly">
+        <h1 class="font-bold"><?= $args['price'] ?> EGP</h1>
+        <?php if ($args['on_sale']) { ?>
+            <h1 class="font-bold line-through"><?= $args['sale_price'] ?> EGP</h1>
+        <?php } ?>
+    </div>
+</a>
+        -->
